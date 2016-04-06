8fd32499c092a828c65ed63b2fedbd2fa9e10611
amirubin87 <amirubin87@walla.com>
2016-04-06 10:58:44 +0300

    Add float cast to F1

--- Tools/AverageF1.py
+++ Tools/AverageF1.py
@@ -2,15 +2,15 @@ __author__ = 'Amir Rubin'
 #From: 'Overlapping community detection at scale: a nonnegative matrix factorization approach'
 
 def precision(guess,truth):
-    return len(guess.intersection(truth))/len(truth)
+    return (float)(len(guess.intersection(truth)))/len(truth)
 
 def recall(guess,truth):
-    return len(guess.intersection(truth))/len(guess)
+    return (float)(len(guess.intersection(truth)))/len(guess)
 
 def H(a,b):
     if a+b==0:
         return 0
-    return 2*a*b/(a+b)
+    return (float)(2*a*b)/(a+b)
 
 def F1(guess,truth):
     return H(precision(guess,truth),recall(guess,truth))
@@ -32,16 +32,19 @@ def FindBestMatchingInComms1(trueComm, Comms1):
     return best
 
 def CalcPartialAverageF1Part1(Comms1,GroundTruth):
-    ans = 0
+    ans = (float)(0)
     for c1 in Comms1:
         ans = ans + FindBestMatchingInGroundTruth(c1, GroundTruth)
-    return ans / (2*len(Comms1))
+        print(c1)
+        print(ans)
+    return (float)(ans) / (2*len(Comms1))
 
 def CalcPartialAverageF1Part2(Comms1, GroundTruth):
-    ans = 0
+    ans = (float)(0)
     for trueComm in GroundTruth:
         ans = ans + FindBestMatchingInComms1(trueComm, Comms1)
-    return ans / (2*len(GroundTruth))
+
+    return (float)(ans) / (2*len(GroundTruth))
 	
 def AverageF1(Comms1Path,GroundTruthPath, outputPath):
     Comms1File = open(Comms1Path, 'r')
